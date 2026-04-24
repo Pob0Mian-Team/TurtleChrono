8ed71eb02432609d627985adbba6406158692193
Tianzong Cheng <oierprime@outlook.com>
2026-04-25 02:45:56 +0800

    feat: rewrite layout to three-column grid and remove old ChartPanel

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -2,7 +2,7 @@ import { FocusWrapper } from './components/FocusWrapper';
 import { Sidebar } from './components/Sidebar';
 import { MapPanel } from './components/MapPanel';
 import { GForcePanel } from './components/GForcePanel';
-import { ChartPanel } from './components/ChartPanel';
+import { ChartContainer } from './components/ChartContainer';
 import { PlaybackBar } from './components/PlaybackBar';
 import { LapInfoPanel } from './components/LapInfoPanel';
 import styles from './App.module.css';
@@ -13,19 +13,19 @@ export default function App() {
       <div className={styles.sidebar}>
         <Sidebar />
       </div>
-      <FocusWrapper panelId="map" className={styles.mapPanel}>
-        <MapPanel />
-      </FocusWrapper>
-      <div className={styles.middleSection}>
-        <div className={styles.leftColumn}>
-          <FocusWrapper panelId="gforce">
-            <GForcePanel />
-          </FocusWrapper>
+      <div className={styles.leftColumn}>
+        <FocusWrapper panelId="map" className={styles.mapPanel}>
+          <MapPanel />
+        </FocusWrapper>
+        <FocusWrapper panelId="gforce" className={styles.gforcePanel}>
+          <GForcePanel />
+        </FocusWrapper>
+        <div className={styles.lapInfoPanel}>
           <LapInfoPanel />
         </div>
-        <FocusWrapper panelId="chart" className={styles.chartPanel}>
-          <ChartPanel />
-        </FocusWrapper>
+      </div>
+      <div className={styles.chartColumn}>
+        <ChartContainer />
       </div>
       <div className={styles.playbackBar}>
         <PlaybackBar />
